f7a9672198feb745c6a5017b43b2f8c7e4069ac8
Kamila W <kamila.wilczynska92@gmail.com>
2024-04-22 09:33:13 +0100

    creating const array of html elements/removing unnecesarry sctipts

--- js/products.js
+++ js/products.js
@@ -1,11 +1,11 @@
 
 
 //find search input
-var searchInput = document.querySelector(".search");
+const searchInput = document.querySelector(".search");
 searchInput.addEventListener("input", updateSearchValue);
 
 // initial search value, which will be empty
-var searchValue = "";
+let searchValue = "";
 
 // check what will be searched
 function updateSearchValue() {
@@ -16,7 +16,7 @@ function updateSearchValue() {
 	//console.log("You searched for: " + searchValue);
 
 	// show or hide // class created previously
-	var productElements = document.querySelectorAll(".product");
+	const productElements = document.querySelectorAll(".product");
 	//loop through all products 
 	productElements.forEach(showOrHideProduct);
 }
@@ -32,7 +32,7 @@ function showOrHideProduct(productElement) {
 	} else {
 
 		// get the name of the user from its user-name element
-		var productName = productElement.querySelector(".product-name").textContent.toLowerCase();
+		const productName = productElement.querySelector(".product-name").textContent.toLowerCase();
 		if (productName.includes(searchValue)) {
 			productElement.classList.remove("hide");
 		} else {
@@ -51,11 +51,12 @@ document.addEventListener("DOMContentLoaded", function () {
 	const products = document.querySelectorAll(".product");
 	const productsToShow = 6; // Change this value to the number of products you want to show initially
 	let isShowingAll = false;
+	const productArray = Array.from(products);
 
 	// Hide products beyond the initial number
 	//By using Array.from(products), we convert the NodeList into a true array, which allows us to use array methods like forEach() on it. 
 	// Array.from() is a built-in JavaScript method that creates a new, shallow-copied Array instance from an array-like or iterable object.
-	Array.from(products).forEach((product, index) => {
+	productArray.forEach((product, index) => {
 		if (index >= productsToShow) {
 			product.style.display = "none";
 		}
@@ -64,16 +65,16 @@ document.addEventListener("DOMContentLoaded", function () {
 	showMoreButton.addEventListener("click", function () {
 		if (isShowingAll) {
 			// If all products are already shown, hide the additional ones
-			Array.from(products).forEach((product, index) => {
+			productArray.forEach((product, index) => {
 				if (index >= productsToShow) {
 					product.style.display = "none";
 				}
 			});
-			showMoreButton.textContent = "Show More";
+
 			isShowingAll = false;
 		} else {
 			// If not all products are shown, display all of them
-			Array.from(products).forEach(product => {
+			productArray.forEach(product => {
 				product.style.display = "block";
 			});
 			showMoreButton.textContent = "Show Less";
